fa2864c6869e1629e66d273b61856b18f195db75
Pedro Vanzella <pedro@pedrovanzella.com>
2016-10-09 17:31:51 -0300

    Add check in flow (possibly incomplete)

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,22 @@
+class TicketsController < ApplicationController
+  before_action :set_ticket, only: [:update]
+  def index
+    @tickets = Ticket.where(user: current_user)
+  end
+
+  def update
+    if @ticket.update(allowed_params)
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def allowed_params
+    params.permit :checked_in
+  end
+
+  def set_ticket
+    @ticket = Ticket.find(params[:id])
+  end
+end
