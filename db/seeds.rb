2eda4f06d9ded9a52e439e63cf239ce6380b4651
Pedro Vanzella <pedro@pedrovanzella.com>
2016-10-09 15:48:37 -0300

    Create Airport model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+User.create(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+Airport.create(code: 'POA', city: 'Porto Alegre')
+Airport.create(code: 'GRU', city: 'Guarulhos')
