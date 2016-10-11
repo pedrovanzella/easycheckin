a93757fa6c577ea4993b653645b70f9822bd20ca
Pedro Vanzella <pedro@pedrovanzella.com>
2016-10-11 09:52:42 -0300

    Only allow web console on dev env

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module Myapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.web_console.whitelisted_ips = '172.21.0.1'
   end
 end
